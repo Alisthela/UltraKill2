08b34ba434fff5e22e88fb27a1b6c9b5d4859f1c
Charlie Daly <CharlieLewisDaly@gmail.com>
2023-09-18 18:22:04 +0930

    debug

---
+++ Ultrakill 2/Assets/Scripts/UI And GUI/Shop.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Shop : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    private bool IsShopOpen = false;
+
+    [SerializeField] GameObject Obj_ShopMenu;
+    
+    public TMP_Text MoneyText;
+    public TMP_Text AmmoText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Obj_ShopMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoneyText.text = "$" + gameManager.MoneyAmount.ToString();
+        AmmoText.text = "Ammo:" + gameManager.AmmoAmount.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (IsShopOpen == false)
+            {
+                OpenMenu();
+            }
+            else if (IsShopOpen == true)
+            {
+                CloseMenu();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            gameManager.MoneyAmount += 10;
+        }
+    }
+
+    public void OpenMenu()
+    {
+        Obj_ShopMenu.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        IsShopOpen = true;
+    }
+
+    public void CloseMenu()
+    {
+        Obj_ShopMenu.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        IsShopOpen = false;
+    }
+}
---
+++ Ultrakill 2/Assets/Scripts/Utilities/GameManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public float MoneyAmount = 0f;
+    public float AmmoAmount = 0f;
+
+    //GameState stuff
+    private float m_gameTime = 0;
+    public float GameTime { get { return m_gameTime; } }
+
+    public enum GameState
+    {
+        Start,
+        Playing,
+        Pause,
+        GameOver
+    };
+
+    public GameState m_GameState;
+
+    public GameState State { get { return m_GameState; } }
+
+    public TMP_Text m_MessageText;
+
+   // public Button m_NewGameButton;
+
+
+    public TMP_Text Txt_Money;
+
+    bool isGameOver = false;
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        m_GameState = GameState.Start;
+    }
+
+    private void Start()
+    {
+       // m_NewGameButton.gameObject.SetActive(false);
+        m_MessageText.gameObject.SetActive(true);
+        m_MessageText.text = "Press Enter/Return to start";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (m_GameState)
+        {
+            case GameState.Start:
+                if (Input.GetKeyUp(KeyCode.Return))
+                {
+                    m_MessageText.text = "";
+                    m_MessageText.gameObject.SetActive(false);
+                    m_GameState = GameState.Playing;
+                }
+                break;
+
+            case GameState.Playing:
+                Cursor.lockState = CursorLockMode.Locked;
+
+                if (isGameOver == true)
+                {
+                    m_GameState = GameState.GameOver;
+
+                    Cursor.lockState = CursorLockMode.None;
+
+                    Time.timeScale = 0f;
+                }
+                break;
+
+            case GameState.Pause:
+                Time.timeScale = 0f;
+                Cursor.lockState = CursorLockMode.None;
+
+                break;
+            case GameState.GameOver:
+                if (Input.GetKeyUp(KeyCode.Return) == true)
+                {
+                    m_gameTime = 0;
+                    m_GameState = GameState.Playing;
+                    m_MessageText.text = "";
+                }
+                break;
+
+        }
+
+    }
+
+}
