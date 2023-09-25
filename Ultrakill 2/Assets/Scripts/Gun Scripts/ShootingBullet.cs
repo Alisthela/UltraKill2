d858ae7cfa62f4ca2bd4eb887b4385340c8257a4
AIE\s220524 <solding.lukaz@gmail.com>
2023-09-25 20:16:32 +0930

    Recreated Gun Scripts again

--- Ultrakill 2/Assets/Scripts/Gun Scripts/ShootingBullet.cs
+++ Ultrakill 2/Assets/Scripts/Gun Scripts/ShootingBullet.cs
@@ -7,18 +7,60 @@ public class ShootingBullet : MonoBehaviour
     public Transform spawnPoint;
     public GameObject bullet;
     public GameObject player;
-    public float speed = 5f;
+    public GunData gunData;
+
+    private void Start()
+    {
+        gunData.currentAmmo = gunData.magSize;
+        gunData.secondsSinceFired = gunData.fireRate;
+    }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-            ShootBullet();
+        if ((gunData.currentAmmo <= 0 || Input.GetKeyDown(KeyCode.R)) && !gunData.reloading)
+        {
+            gunData.reloading = true;
+            StartCoroutine(Reload());
+        }
+        else
+        {
+            if (gunData.secondsSinceFired > 0 && !gunData.shootDelay)
+                if (Input.GetKeyDown(KeyCode.Mouse0) && !gunData.reloading)
+                {
+                    ShootBullet();
+                    gunData.shootDelay = true;
+                    StartCoroutine(ShootDelay());
+                }
+            if (Input.GetKeyDown(KeyCode.Mouse1) && !gunData.usingAbility)
+                UseAbility();
+        }
     }
 
     private void ShootBullet()
     {
+        bullet.transform.rotation = Quaternion.Euler(bullet.transform.rotation.x, bullet.transform.rotation.y, player.transform.rotation.z);
         GameObject cB = Instantiate(bullet, spawnPoint.position, bullet.transform.rotation);
         Rigidbody rb = cB.GetComponent<Rigidbody>();
-        rb.AddForce(spawnPoint.forward * speed, ForceMode.Impulse);
+        rb.AddForce(spawnPoint.forward * gunData.speed, ForceMode.Impulse);
+        gunData.currentAmmo -= 1;
+    }
+
+    private IEnumerator ShootDelay()
+    {
+        yield return new WaitForSeconds(gunData.fireRate);
+        gunData.secondsSinceFired = gunData.fireRate;
+        gunData.shootDelay = false;
+    }
+
+    private IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(gunData.reloadTime);
+        gunData.currentAmmo = gunData.magSize;
+        gunData.reloading = false;
+    }
+
+    private void UseAbility()
+    {
+
     }
 }
