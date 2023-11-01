6eced5850944b6d01734ff6146240908a8f9ab09
Alis <alisthela@outlook.com>
2023-11-01 20:28:24 +1030

    ee

--- Ultrakill 2/Assets/Alis/Scripts/UIShowHealth.cs
+++ Ultrakill 2/Assets/Alis/Scripts/UIShowHealth.cs
@@ -20,6 +20,6 @@ public class UIShowHealth : MonoBehaviour
     {
         currentHealthTXT = playerHealth.currentHealth;
         maxHealthTXT = playerHealth.startHealth;
-        healthText.text = "Health: " + currentHealthTXT.ToString() + "/" + maxHealthTXT.ToString();
+        healthText.text = currentHealthTXT.ToString() + "/" + maxHealthTXT.ToString();
     }
 }
